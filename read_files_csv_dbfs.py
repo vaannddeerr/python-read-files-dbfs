22da55cd47d5ce952feb94a2050e3d003a44eeae
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-15 05:04:15 +0100

    Ajuste

--- read_files_csv_dbfs.py
+++ read_files_csv_dbfs.py
@@ -12,4 +12,4 @@ df = (spark.read
            .option('inferSchema',True)
            .load('/Volumes/workspace/default/landing/*.xlsx'))
 
-df.write.format('delta').saveAsTable('b_sttpt.base_clie_full')
+df.write.format('delta').saveAsTable('base_clie_full')
