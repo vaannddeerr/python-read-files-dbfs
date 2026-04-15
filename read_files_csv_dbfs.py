ef20b2114665876fccac8405fbf6e5e08a595845
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-15 02:32:46 +0100

    Primeira execução

--- read_files_csv_dbfs.py
+++ read_files_csv_dbfs.py
@@ -6,6 +6,7 @@ df = (spark.read
            .format('csv')
            .option('header',True)
            .option('delimiter',',')
+           .option('inferSchema',True)
            .load('dbfs:/Volumes/workspace/default/landing/*.csv'))
 
 df.writer.format('delta').saveAsTable('b_sttpt.base_clie_full')
