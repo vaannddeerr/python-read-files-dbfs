5bae089f9c5565c992c25a9adcc6ff188715f415
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-15 05:22:33 +0100

    Ajuste

--- read_files_csv_dbfs.py
+++ read_files_csv_dbfs.py
@@ -7,9 +7,10 @@ spark = spark_session()
 
 df = (spark.read
            .format('excel')
-           .option('header',True)
+           .option('header','true')
            .option('delimiter',',')
-           .option('inferSchema',True)
+           .option('inferSchema','true')
+           .option('treatEmptyValuesAsNulls','true')
            .load('/Volumes/workspace/default/landing/*.xlsx'))
 
 df.write.format('delta').mode('overwrite').saveAsTable('base_clie_full')
