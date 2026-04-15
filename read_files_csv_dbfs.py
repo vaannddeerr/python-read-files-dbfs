33fd94e91102238a352aac85a7bb4261867ff27e
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-15 05:42:48 +0100

    Ajuste

--- read_files_csv_dbfs.py
+++ read_files_csv_dbfs.py
@@ -4,7 +4,7 @@ from spark_dataset import spark_session
 
 spark = spark_session()
 
-
+print('📖 Lendo arquivos')
 df = (spark.read
            .format('excel')
            .option('header','true')
@@ -13,7 +13,14 @@ df = (spark.read
            .option('treatEmptyValuesAsNulls','true')
            .load('/Volumes/workspace/default/landing/*.xlsx')).filter(F.col('_c1') != '_c1')
 
+print('📖 Leitura finalizada com sucesso ✔️')
+
+
+print('✍️ Escrevendo dados')
 (df.write
    .format('delta')
    .mode('overwrite')
    .saveAsTable('base_clie_full'))
+
+print('✔️ Tabela salva com sucesso...')
+
